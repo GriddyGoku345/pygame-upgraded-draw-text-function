8be820ea91834a6ba8fc6b3e9262f78d1073fb07
GriddyGoku345 <bissonetteis924@northwested.org>
2025-03-11 11:19:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import config  # Import the config module
 
 def init_game():
     pygame.init()
+    
+    pygame.font.init()  # Initialize fonts here
+
     screen = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT))  # Use constants from config
     pygame.display.set_caption(config.TITLE)
     return screen
@@ -19,15 +22,42 @@ def handle_events():
                 return False
     return True
 
+
+def draw_text():
+    pass
+
 def main():
 
     screen = init_game()
     clock = pygame.time.Clock() # Initialize the clock object
-    
+
+    font_name = "FreeMono.ttf"
+    font_color1 = config.RED  # Use a color from the config
+    font_color2 = config.MIDNIGHT_PURPLE
+    font_color3 = config.DARKORANGE
+    font_size_normal = 36
+    font_size_bold_italic = 30
+    font_size_custom = 48
+
+    # Using a True Type Font (.ttf)
+    custom_font_name = 'Jacquard12-Regular.ttf'
+
+    # Define x,y coordinate pairs for top left of text rectangle (stamp)
+    text_position_1 = [50, 50]
+    text_position_2 = [225, 135]
+    text_position_3 = [220, 370]
+
+
     running = True
     while running:
         running = handle_events()
         screen.fill(config.WHITE)  # Use color from config
+
+        draw_text(screen, "Hello World!", font_size_normal, font_name, font_color1, text_position_1, italic=True)
+        draw_text(screen, "Text which is bold and italic.", font_size_bold_italic, font_name, font_color2, text_position_2, italic=True, bold=True)
+
+
+
         pygame.display.flip()
 
         # Limit frame rate to certain number of frames per second (FPS)
