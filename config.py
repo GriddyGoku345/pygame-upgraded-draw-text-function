6022cb514cbe7a744b604998b2016501932b6aa5
GriddyGoku345 <bissonetteis924@northwested.org>
2025-03-11 10:40:53 -0400

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,13 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 DARKORANGE = (255, 140, 0)
+LIGHTBLUE = (144,213,255)
+MIDNIGHT_PURPLE = (45, 3, 55)
+GRAY = (150, 150, 150)
+SKY_BLUE = (135, 206, 235)
+FOREST_GREEN = (1, 68, 33)
+SLIGHTLY_LIGHTER_FOREST_GREEN = (34, 139, 34)
+DARK_GREY = (105, 105, 105)
 
 # Game window dimensions
 WINDOW_WIDTH = 800
@@ -14,7 +21,7 @@ WINDOW_HEIGHT = 600
 
 # Window title (caption)
 # Update the window title as needed
-TITLE = "Pygame TEMPLATE 2.0"
+TITLE = "thy end is drawing near"
 
 # Frame rate (frames per second)
 FPS = 60
